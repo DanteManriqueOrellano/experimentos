6252aed207d2d59e60ffba5913f4fcadca25dcb2
DanteManriqueOrellano <relaxurbano@me.com>
2020-06-02 05:08:20 -0500

    joder

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { ShellComponent } from './shell/shell.component';
+import { AModule } from '../a/a.module';
+import { BModule } from '../b/b.module';
+import { CModule } from '../c/c.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ShellComponent],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    AModule,
+    BModule,
+    CModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[ShellComponent]
 })
